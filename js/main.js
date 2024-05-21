04eec2be88b5edfa10fbddd922a658e7cd48c7c7
Davi Nascimento <davileite2007@gmail.com>
2024-05-21 12:34:08 -0300

    Adicionada tradução em inglês!

--- js/main.js
+++ js/main.js
@@ -1,24 +1,55 @@
 tela = {
-    main: true,
+    idioma: true,
+    main: false,
     sobre: false,
     colecao: false,
-    colecaoInitial: $('.card-box').html()
 }
 
 $(() => {
-    $('#colecao').click(function(){
-        if(!tela.colecao){
-            tela.colecao = true;
-            tela.main = false;
-
-            $('.main-wraper').fadeOut(500);
+    $('#flag-pt').click(function(){
+        if(tela.idioma){
+            tela.idioma = false;
+            $('#idioma-ingles').remove();
+            $('.idiomas').fadeOut(500);
             setTimeout(() => {
-                $('.collection-box-wraper').fadeIn(500)
+                $('#idioma-portugues').fadeIn(500);
+                tela.main = true
+                ativarMain();
+                ativarCardBox();
             }, 500);
         }
     })
 
-    $('#github').click(function(){
-        window.open('https://github.com/JohnnyW23', '_blank')
+    $('#flag-ing').click(function(){
+        if(tela.idioma){
+            tela.idioma = false;
+            $('#idioma-portugues').remove();
+            $('.idiomas').fadeOut(500);
+            setTimeout(() => {
+                $('#idioma-ingles').fadeIn(500);
+                tela.main = true;
+                ativarMain();
+                ativarCardBox();
+            }, 500);
+        }
     })
+
+    function ativarMain(){
+        $('#colecao').click(function(){
+            if(!tela.colecao){
+                tela.colecao = true;
+                tela.main = false;
+                tela.colecaoInitial = $('.card-box').html();
+    
+                $('.main-wraper').fadeOut(500);
+                setTimeout(() => {
+                    $('.collection-box-wraper').fadeIn(500)
+                }, 500);
+            }
+        })
+    
+        $('#github').click(function(){
+            window.open('https://github.com/JohnnyW23', '_blank')
+        })
+    }
 })
