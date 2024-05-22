6a418e7a18ee1955859b2ff7f7da9084c7e7ce76
Davi Nascimento <davileite2007@gmail.com>
2024-05-22 12:59:29 -0300

    Página com tradução 100% dinâmica

--- js/main.js
+++ js/main.js
@@ -3,18 +3,49 @@ tela = {
     main: false,
     sobre: false,
     colecao: false,
+    portugues: {
+        sobre: [
+            'SOBRE O PROJETO',
+            'O que significa esse projeto e o porquê dele ter sido feito.'
+        ],
+        colecao: [
+            'CARTAS',
+            'Confira todas as cartas que a coleção possui!'
+        ],
+        github: [
+            'GITHUB',
+            'Confira outros projetos em meu perfil no GitHub!'
+        ],
+        voltar_btn: 'VOLTAR'
+    },
+    ingles: {
+        sobre: [
+            'ABOUT THE PROJECT',
+            'What is the meaning of this project and why I made it.'
+        ],
+        colecao: [
+            'CARDS',
+            'Check out the complete collection of available cards!'
+        ],
+        github: [
+            'GITHUB',
+            'Check out other projects on my GitHub profile!'
+        ],
+        voltar_btn: 'RETURN'
+    }
 }
 
 $(() => {
     $('#flag-pt').click(function(){
         if(tela.idioma){
             tela.idioma = 'portugues';
-            $('#idioma-ingles').remove();
-
+            traduzirMain();
             construirCartas();
+
             $('.idiomas-wraper').fadeOut(500);
             setTimeout(() => {
-                $('#idioma-portugues').fadeIn(500);
+                $('.page-wraper').fadeIn(500);
+                $('.page-wraper').css('display', 'grid');
                 tela.main = true
                 ativarMain();
                 ativarCardBox();
@@ -25,18 +56,52 @@ $(() => {
     $('#flag-ing').click(function(){
         if(tela.idioma){
             tela.idioma = 'ingles';
-            $('#idioma-portugues').remove();
+            traduzirMain();
             construirCartas();
 
             $('.idiomas-wraper').fadeOut(500);
             setTimeout(() => {
-                $('#idioma-ingles').fadeIn(500);
+                $('.page-wraper').fadeIn(500);
+                $('.page-wraper').css('display', 'grid');
                 tela.main = true;
                 ativarMain();
                 ativarCardBox();
             }, 500);
         }
     })
+
+    function traduzirMain(){
+        if(tela.idioma == 'portugues'){
+            $('#sobre .sessao-descricao').append('\
+                <h2>' + tela.portugues.sobre[0] + '</h2>\
+                <p>' + tela.portugues.sobre[1] + '</p>\
+            ');
+            $('#colecao .sessao-descricao').append('\
+                <h2>' + tela.portugues.colecao[0] + '</h2>\
+                <p>' + tela.portugues.colecao[1] + '</p>\
+            ');
+            $('#github .sessao-descricao').append('\
+                <h2>' + tela.portugues.github[0] + '</h2>\
+                <p>' + tela.portugues.github[1] + '</p>\
+            ');
+            $('.voltar-btn').append('<h2>' + tela.portugues.voltar_btn  + '</h2>');
+
+        }else if(tela.idioma == 'ingles'){
+            $('#sobre .sessao-descricao').append('\
+                <h2>' + tela.ingles.sobre[0] + '</h2>\
+                <p>' + tela.ingles.sobre[1] + '</p>\
+            ');
+            $('#colecao .sessao-descricao').append('\
+                <h2>' + tela.ingles.colecao[0] + '</h2>\
+                <p>' + tela.ingles.colecao[1] + '</p>\
+            ');
+            $('#github .sessao-descricao').append('\
+                <h2>' + tela.ingles.github[0] + '</h2>\
+                <p>' + tela.ingles.github[1] + '</p>\
+            ');
+            $('.voltar-btn').append('<h2>' + tela.ingles.voltar_btn  + '</h2>');
+        }
+    }
 
     function ativarMain(){
         $('#colecao').click(function(){
